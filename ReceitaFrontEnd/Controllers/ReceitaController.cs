a7e2dcea1c09d390aeb2dc020e8a5d652b883366
Kiane Noronha <kianenoronha24@gmail.com>
2023-08-16 20:07:22 -0300

    first commit

---
+++ ReceitaFrontEnd/Controllers/ReceitaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReceitaFrontEnd.Controllers
+{
+    public class ReceitaController : Controller
+    {
+        // GET: Receita
+        public ActionResult Index()
+        {
+
+
+
+
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+
+
+
+
+            return View();
+        }
+    }
+}
