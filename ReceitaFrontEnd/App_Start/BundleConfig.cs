a595069e3c00d952b84ba3f771d4ed6f8cbb5ad9
Kiane Noronha <kianenoronha24@gmail.com>
2023-08-16 21:43:04 -0300

    Excluir

--- ReceitaFrontEnd/App_Start/BundleConfig.cs
+++ ReceitaFrontEnd/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace ReceitaFrontEnd
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/ReceitaScript.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
---
+++ ReceitaFrontEnd/Scripts/ReceitaScript.js
@@ -0,0 +1,20 @@
+﻿window.onload = function () {
+
+    $(".test").on("click", function (e) {
+        $(this).toggleClass('bottomhover');
+        toggleMenuContagemDelete();
+    });
+
+}
+
+function toggleMenuContagemDelete() {
+    var isOpen = $(".test").hasClass('bottomhover');
+
+    if (isOpen) {
+        if (!$(".menu").hasClass("active")) {
+            $(".menu").addClass("active");
+        }
+    } else {
+        $(".menu").removeClass("active");
+    }
+}
