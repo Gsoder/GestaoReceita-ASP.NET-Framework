3cc63a8a88760bf5ea2d24d3f253cb222d76bcf7
Kiane Noronha <kianenoronha24@gmail.com>
2023-08-21 20:09:43 -0300

    Controller

---
+++ ReceitaFrontEnd/Controllers/ReceitaController.cs
@@ -0,0 +1,44 @@
+﻿using ReceitaFrontEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReceitaFrontEnd.Controllers
+{
+    public class ReceitaController : Controller
+    {
+        [HttpPost]
+        public JsonResult test(ReceitaViewModel model)
+        {
+           return Json(model);
+        }
+
+        public ActionResult Index()
+        {
+            List<ReceitaViewModel> list = new List<ReceitaViewModel>();
+            Random random = new Random();
+
+            for (int i = 1; i <= 20; i++)
+            {
+                string nomeReceita = $"Receita {i}";
+                int numeroAleatorio = random.Next(100, 401);
+                ReceitaViewModel receita = new ReceitaViewModel { Id = i, Receita = nomeReceita, preco = numeroAleatorio }; ;
+                list.Add(receita);
+            }
+
+            return View(list);
+        }
+
+        public ActionResult Cadastro()
+        {
+
+
+
+
+            return View();
+        }
+    }
+}
---
+++ ReceitaFrontEnd/Scripts/ReceitaScript.js
@@ -0,0 +1,91 @@
+﻿window.onload = function () {
+    const receitas = [];
+
+    $(".test").on("click", function (e) {
+        $(this).toggleClass('bottomhover');
+        const divPai = this.parentElement;
+        const divPai1 = divPai.parentElement;
+        receitas.push(divPai1);
+        console.log(receitas);
+        toggleMenuContagemDelete();
+    });
+
+
+    /*$("#teste").on("click", function (e) {
+        var a = document.getElementById('teste').value;
+        $.ajax({
+            url: ReceitaController,
+            method: "POST",
+            data: { "TEST": A },
+            error:
+                function (r) {
+                    console.log(r);
+                },
+            success:
+                function (r) {
+                    console.log(r)
+                    console.log()
+                }
+        });
+    });*/
+
+    $("#EXCLUIR").on("click", function (e) {
+        sla = receitas.length;
+        i = 0
+        while (i < sla) {
+
+            console.log(receitas[i])
+            receitas[i].remove();
+            $(".menu").removeClass("active");
+            i++;
+
+        }
+
+
+    });
+
+    $("#excluirReceita").on("click", function (e) {
+        const length = 10;
+        const randomString = generateRandomString(length);
+        a = document.getElementById('testSocial');
+        a.innerText = randomString;
+        $(a).css('font-weight', 'bold')
+        $(a).css('font-size', '30px')
+        $(a).css('user-select', 'none')
+    });
+
+ };
+
+    
+  
+
+function toggleMenuContagemDelete() {
+    var isOpen = $(".test").hasClass('bottomhover');
+
+    if (isOpen) {
+        if (!$(".menu").hasClass("active")) {
+            $(".menu").addClass("active");
+        }
+    } else {
+        $(".menu").removeClass("active");
+    }
+
+
+}
+function onClick(e) {
+    e.preventDefault();
+    grecaptcha.enterprise.ready(async () => {
+        const token = await grecaptcha.enterprise.execute('6LdD-78nAAAAAFsoghhAlZeIJaJuapMXSopJVt_K', { action: 'LOGIN' });
+    });
+}
+function generateRandomString(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        randomString += chars.charAt(randomIndex);
+    }
+
+    return randomString;
+}
